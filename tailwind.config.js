d742766f2c624c9844a55ff956b541743a9a01f0
Galih Adhi Kusuma <noxturne.production@gmail.com>
2022-05-27 18:46:13 +0700

    refactor and fix bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   mode: "jit",
   content: ["./view/**/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        popUp : {
+          '0%': {transform : 'translateY(0), opacity: 0'},
+          '100%': {transform: 'translateY(1rem), opacity: 100'}
+        },
+      },
+      animation: {
+        'pop-up' : 'popUp 2s linear'
+      }
+    },
   },
   plugins: [],
 }
